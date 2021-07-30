262f85fda41b6993ea9c3f642519f55e6344627a
NikonorIvanovich <bid93@mail.ru>
2021-07-30 15:07:43 +0300

    add worked matomo

--- pages/_app.js
+++ pages/_app.js
@@ -3,20 +3,38 @@ import "../styles/globals.css";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import * as gtag from "../lib/gtag";
+import React from "react";
+import App from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+import { init } from "@socialgouv/matomo-next";
 
-  return <Component {...pageProps} />;
+const MATOMO_URL =
+  "https://nextjsdl2je093qblacknumberthreevercelapp.matomo.cloud/";
+const MATOMO_SITE_ID = 1;
+
+class MyApp extends App {
+  componentDidMount() {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
 
 export default MyApp;
+// <!-- Matomo -->
+// <script type="text/javascript">
+//   var _paq = window._paq = window._paq || [];
+//   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+//   _paq.push(['trackPageView']);
+//   _paq.push(['enableLinkTracking']);
+//   (function() {
+//     var u="https://nextjsdl2je093qblacknumberthreevercelapp.matomo.cloud/";
+//     _paq.push(['setTrackerUrl', u+'matomo.php']);
+//     _paq.push(['setSiteId', '1']);
+//     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+//     g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/nextjsdl2je093qblacknumberthreevercelapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+//   })();
+// </script>
+// <!-- End Matomo Code -->
